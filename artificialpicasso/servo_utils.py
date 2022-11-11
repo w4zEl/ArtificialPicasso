4b6571ef8843cff290f741d63934c32e6b0f3e70
ProximityTrigger <proximitytrigger@gmail.com>
2022-11-11 16:27:05 -0500

    servo_utils.py

---
+++ artificialpicasso/servo_utils.py
@@ -0,0 +1,21 @@
+from adafruit_pca9685 import PCA9685
+from adafruit_motor.servo import Servo
+import board
+import time
+
+pca = PCA9685(board.I2C())
+pca.frequency = 50
+min_pulse, max_pulse = 500, 2600
+
+
+def make_servo(channel: int) -> Servo:
+    return Servo(pca.channels[channel], min_pulse=min_pulse, max_pulse=max_pulse)
+
+
+def rotate(s: Servo, angle: float, seconds: float = 0.5, increment: float = 1) -> None:
+    delay = seconds / (abs(angle - s.angle) / increment)
+    delta = increment if s.angle < angle else -increment
+    cap = min if s.angle < angle else max
+    while abs(s.angle - angle) > .5:
+        s.angle = cap(s.angle + delta, angle)
+        time.sleep(delay)
