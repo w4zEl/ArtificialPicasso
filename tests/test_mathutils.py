dd0fc72866b0a45110a3df5200bc401513bc5bb1
Adib Raed <araedcanada@gmail.com>
2022-11-11 15:26:17 -0500

    Added tests to check where between is false

--- tests/test_mathutils.py
+++ tests/test_mathutils.py
@@ -11,3 +11,14 @@ from artificialpicasso.mathutils import between
 ])
 def test_between(b1, b2, val):
     assert between(b1, b2, val)
+
+
+@pytest.mark.parametrize("b1, b2, val", [
+    (1, 9, 10),
+    (2, 17, -1),
+    (8, 5, 0),
+    (0, 0, 5),
+    (-13, -7, -5)
+])
+def test_not_between(b1, b2, val):
+    assert not between(b1, b2, val)
