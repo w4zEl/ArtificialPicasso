cb301db1a5ddba43a17cbc4e64bd5eded7708ff4
ProximityTrigger <proximitytrigger@gmail.com>
2022-11-18 12:03:10 -0500

    ArmTestTemplate.py: add __main__ guard.

--- artificialpicasso/ArmTestTemplate.py
+++ artificialpicasso/ArmTestTemplate.py
@@ -7,14 +7,15 @@ from servo_utils import make_servo
 import time
 import atexit
 
-# lengths are in cm, but any unit is fine as long as they are all consistent
-paper_delta_x = paper_delta_y = 3.5
-paper_width = 27.6
-paper_height = 21.3
-controller = ArmController(arm1len=20, arm2len=22.3, arm1servo=make_servo(5), arm2servo=make_servo(0),
-                           tip_servo=make_servo(1))
-atexit.register(controller.reset_positions)
+if __name__ == '__main__':
+    # lengths are in cm, but any unit is fine as long as they are all consistent
+    paper_delta_x = paper_delta_y = 3.5
+    paper_width = 27.6
+    paper_height = 21.3
+    controller = ArmController(arm1len=20, arm2len=22.3, arm1servo=make_servo(5), arm2servo=make_servo(0),
+                               tip_servo=make_servo(1))
+    atexit.register(controller.reset_positions)
 
 
-def move_to_paper_coords(x: float, y: float, *rest) -> None:
-    controller.move_to(x + paper_delta_x, y + paper_delta_y, *rest)
+    def move_to_paper_coords(x: float, y: float, *rest) -> None:
+        controller.move_to(x + paper_delta_x, y + paper_delta_y, *rest)
