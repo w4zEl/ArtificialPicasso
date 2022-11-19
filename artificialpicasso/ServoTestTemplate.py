746ec3a025e3f726196a4af27cf3c1eb47ebf9f8
ProximityTrigger <proximitytrigger@gmail.com>
2022-11-18 23:44:35 -0500

    ServoTestTemplate.py: fix documentation.

--- artificialpicasso/ServoTestTemplate.py
+++ artificialpicasso/ServoTestTemplate.py
@@ -1,8 +1,11 @@
 """
-Used for quick interactive servo testing on the Raspberry Pi with I2C PCA9685
-e.g. by running:
+Used for quick interactive servo testing on the Raspberry Pi with I2C PCA9685.
 
-python -i ServoTestTemplate.py
+For example, by running:
+
+.. code-block::
+
+    python -i ServoTestTemplate.py
 """
 
 import time
@@ -18,14 +21,14 @@ max_pulse = 2600
 makeServo = lambda channel: servo.Servo(pca.channels[channel], min_pulse=min_pulse, max_pulse=max_pulse)
 
 
-def rotate(s: servo, angle: float, ms: float, increment: float = 1) -> None:
+def rotate(s: servo.Servo, angle: float, ms: float, increment: float = 1) -> None:
     """Rotates the given servo to the desired angle in the desired amount of time.
 
     Args:
-        s (Servo): The servo to rotate 
-        angle (float): The angle in degrees to rotate. 0 <= angle <= 180
-        seconds (float, optional): Time in seconds to rotate the arm in. Defaults to 0.5.
-        increment (float, optional): Moves the arm in increments of this size in degrees. Defaults to 1 degree.
+        s: The servo to rotate
+        angle: The angle in degrees to rotate. 0 <= angle <= 180
+        ms: Time in milliseconds to rotate the arm in.
+        increment: Moves the arm in increments of this size in degrees. Defaults to 1 degree.
     """
     delay = ms / (abs(angle - s.angle) / increment)
     print(delay)
@@ -38,6 +41,6 @@ def rotate(s: servo, angle: float, ms: float, increment: float = 1) -> None:
 
 
 def stop():
-    """Resets the servos and exits"""
+    """Releases usage of the PCA and exits"""
     pca.deinit()
     exit()
