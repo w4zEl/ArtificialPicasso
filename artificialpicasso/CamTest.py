ab1840eb8a4c1c1ce970118bd1e991a0c069056a
ProximityTrigger <proximitytrigger@gmail.com>
2022-11-24 22:13:56 -0500

    CamTest.py to capture live video stream from camera.

---
+++ artificialpicasso/CamTest.py
@@ -0,0 +1,18 @@
+"""
+Captures live video stream from the camera at device index 0 (the default one).
+"""
+
+import cv2
+
+cam = cv2.VideoCapture(0)
+if not cam.isOpened():
+    print('Camera could not be opened')
+    exit(1)
+while 1:
+    ret, frame = cam.read()
+    if not ret:
+        print('Unable to read frame from camera')
+        exit(1)
+    cv2.imshow('Camera', frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
