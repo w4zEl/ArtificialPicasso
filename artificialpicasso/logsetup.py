94be0f818b7ab8d455de281bddc6942174143e51
ProximityTrigger <proximitytrigger@gmail.com>
2022-11-25 00:17:16 -0500

    Remove unneeded print and fix log format string.

--- artificialpicasso/logsetup.py
+++ artificialpicasso/logsetup.py
@@ -1,7 +1,7 @@
 import sys
 from loguru import logger
 log_format = '<green>{time:YYYY-MM-DD HH:mm:ss.SSS}</green> <level>{level:<8}</level> [t={thread}] {extra} ' \
-             '<cyan>{file}</cyan>:<cyan>{function}</cyan>:<cyan>{line}</cyan> - {message} '
+             '<cyan>{file}</cyan>:<cyan>{function}</cyan>:<cyan>{line}</cyan> - {message}'
 logger.remove()
 logger.add(sys.stdout, format=log_format, level='INFO')
 logger.add("logs/artificialpicasso_{time}.log", format=log_format, rotation="100 MB")
