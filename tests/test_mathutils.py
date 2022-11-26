858ea63789dda69a1b8fe9d3b3f41e104261e900
ProximityTrigger <proximitytrigger@gmail.com>
2022-11-26 12:10:48 -0500

    Update spacing.

--- tests/test_mathutils.py
+++ tests/test_mathutils.py
@@ -23,24 +23,24 @@ def test_between(b1, b2, val):
 def test_not_between(b1, b2, val):
     assert not between(b1, b2, val)
 
+
 @pytest.mark.parametrize("a, b, c, angle", [
     (3, 4, 5, 90),
     (5, 8, 9, 84.26082952273322),
-    (7.5, 6.5, 3.5, 27.795772496027972), 
+    (7.5, 6.5, 3.5, 27.795772496027972),
     (1, 1.414, 1, 45.008651662837984),
     (10, 4, 6, 0)
 ])
-def test_cosine_law_find_angle(a, b, c, angle): 
+def test_cosine_law_find_angle(a, b, c, angle):
     assert abs(cosine_law_find_angle(a, b, c) - angle) < 0.000001
 
+
 @pytest.mark.parametrize("a, b, angle, side_found", [
-    (15, 16, 2.5, 1.2070019224128155), 
+    (15, 16, 2.5, 1.2070019224128155),
     (20, 40, 1, 20.006091266156265),
-    (3, 4, 90, 5), 
-    (15, 35.5, 50, 28.296310637322236), 
+    (3, 4, 90, 5),
+    (15, 35.5, 50, 28.296310637322236),
     (4.5, 6.5, 179, 10.999595000119273)
 ])
-
-def test_cosine_law_find_side(a, b, angle, side_found): 
+def test_cosine_law_find_side(a, b, angle, side_found):
     assert abs(cosine_law_find_side(a, b, angle) - side_found) < 0.000001
-
