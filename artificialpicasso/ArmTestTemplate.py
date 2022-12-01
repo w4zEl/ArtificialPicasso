b89c3012fa7e054ec29be13ea285482ff65283cc
ProximityTrigger <proximitytrigger@gmail.com>
2022-12-01 15:05:53 -0500

    Update ArmTestTemplate.py for new larger servos.

--- artificialpicasso/ArmTestTemplate.py
+++ artificialpicasso/ArmTestTemplate.py
@@ -12,8 +12,10 @@ if __name__ == '__main__':
     paper_delta_x = paper_delta_y = 3.5
     paper_width = 27.6
     paper_height = 21.3
-    controller = ArmController(arm1len=20, arm2len=22.3, arm1servo=make_adjusted_servo(3, lambda x: 180 - x),
-                               arm2servo=make_servo(1), tip_servo=make_servo(0))
+    controller = ArmController(arm1len=20.2, arm2len=22.1,
+                               arm1servo=make_adjusted_servo(3, convert_angle=lambda x: 180 - x, max_pulse=2500),
+                               arm2servo=make_adjusted_servo(1, convert_angle=lambda x: 180 - x, max_pulse=2500),
+                               tip_servo=make_servo(0))
     atexit.register(controller.reset_positions)
 
 
