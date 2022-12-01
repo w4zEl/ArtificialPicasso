58ad283e395acf9fc732476c389d359bc333b8bb
ProximityTrigger <proximitytrigger@gmail.com>
2022-12-01 15:04:05 -0500

    servo_utils.py: add safe_rotate, max_angle_delta to ServoWrapper, update rotate2.

--- artificialpicasso/servo_utils.py
+++ artificialpicasso/servo_utils.py
@@ -4,6 +4,8 @@ import board
 import time
 import atexit
 from typing import Callable, Optional
+from math import inf
+from loguru import logger
 
 pca = PCA9685(board.I2C())
 pca.frequency = 50
@@ -16,26 +18,37 @@ def make_servo(channel: int) -> Servo:
     return Servo(pca.channels[channel], min_pulse=min_pulse, max_pulse=max_pulse)
 
 
-def make_adjusted_servo(channel: int, convert_angle: Callable[[float], float],
-                        reverse_convert: Callable[[float], float] = None) -> Servo:
-    return ServoWrapper(pca.channels[channel], min_pulse=min_pulse, max_pulse=max_pulse,
-                        convert_angle=convert_angle, reverse_convert=reverse_convert or convert_angle)
+def make_adjusted_servo(channel: int, **kwargs) -> Servo:
+    return ServoWrapper(pca.channels[channel], min_pulse=kwargs.pop('min_pulse', min_pulse),
+                        max_pulse=kwargs.pop('max_pulse', max_pulse), **kwargs)
 
 
 class ServoWrapper(Servo):
-    def __init__(self, *args, **kwargs):
-        self.convert_angle = kwargs.pop('convert_angle')
-        self.reverse_convert = kwargs.pop('reverse_convert')
+    def __init__(self, *args, convert_angle: Optional[Callable[[float], float]] = None,
+                 reverse_convert: Optional[Callable[[float], float]] = None,
+                 max_angle_delta: float = inf,
+                 **kwargs):
+        self.convert_angle = convert_angle
+        self.reverse_convert = reverse_convert or convert_angle
+        if bool(self.convert_angle) != bool(self.reverse_convert):
+            logger.warning('Only one conversion function given. '
+                           'It is recommended to provide both so the conversion can be reversed.')
+        self.max_angle_delta = max_angle_delta
         super().__init__(*args, **kwargs)
 
     @property
     def angle(self) -> Optional[float]:
         a = super().angle
-        return self.reverse_convert(a) if a else a
+        return self.reverse_convert(a) if a and self.reverse_convert else a
 
     @angle.setter
     def angle(self, new_angle: Optional[int]) -> None:
-        super(__class__, self.__class__).angle.__set__(self, self.convert_angle(new_angle) if new_angle else new_angle)
+        if new_angle and abs(self.angle - new_angle) > self.max_angle_delta:
+            logger.warning(f'Stopped attempt to rotate more than allowed max_angle_delta [{self.max_angle_delta}] at '
+                           f'once! Current angle: {self.angle}, new_angle: {new_angle}')
+            return
+        super(__class__, self.__class__).angle.__set__(self, self.convert_angle(new_angle)
+                                                       if new_angle and self.convert_angle else new_angle)
 
 
 @atexit.register
@@ -60,6 +73,10 @@ def rotate(s: Servo, angle: float, seconds: float = 0.5, increment: float = 1) -
         time.sleep(delay)
 
 
+def increment(angle, delta, target):
+    return angle + delta if angle + delta < target else angle - delta if angle - delta > target else target
+
+
 def rotate2(servo1: Servo, angle1: float, servo2: Servo, angle2: float, seconds: float = 0.5) -> None:
     """
     Rotate two servos simultaneously to reach the desired angle for each in the same amount of time
@@ -69,18 +86,33 @@ def rotate2(servo1: Servo, angle1: float, servo2: Servo, angle2: float, seconds:
     if seconds == 0:
         servo1.angle, servo2.angle = angle1, angle2
         return
-    if abs(servo1.angle - angle1) < abs(servo2.angle - angle2):
+    if abs(servo1.angle - angle1) > abs(servo2.angle - angle2):
         servo1, angle1, servo2, angle2 = servo2, angle2, servo1, angle1
-    rotations = abs(servo1.angle - angle1)
+    increment1 = 1
+    rotations = abs(servo1.angle - angle1) / increment1
     delay1 = seconds / rotations
     increment2 = abs(servo2.angle - angle2) / rotations
     curr_angle1 = servo1.angle
     curr_angle2 = servo2.angle
 
-    def increment(angle, delta, target):
-        return angle + delta if angle + delta < target else angle - delta if angle - delta > target else target
-
     while abs(servo1.angle - angle1) > .5:
-        servo1.angle = curr_angle1 = increment(curr_angle1, 1, angle1)
+        servo1.angle = curr_angle1 = increment(curr_angle1, increment1, angle1)
         servo2.angle = curr_angle2 = increment(curr_angle2, increment2, angle2)
         time.sleep(delay1)
+    servo1.angle = angle1
+    servo2.angle = angle2
+
+
+def safe_rotate(s: Servo, angle: float, delay: float = 0.03) -> None:
+    if delay <= 0:
+        raise ValueError("delay must be positive")
+    if s.angle < angle:
+        while s.angle + 1 < angle:
+            s.angle += 1
+            time.sleep(delay)
+        s.angle = angle
+    else:
+        while s.angle - 1 > angle:
+            s.angle -= 1
+            time.sleep(delay)
+        s.angle = angle
