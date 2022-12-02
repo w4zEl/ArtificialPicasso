eb3779471bb77c5965cc7c6cd337c6438131968b
ProximityTrigger <proximitytrigger@gmail.com>
2022-12-02 11:07:34 -0500

    imgutils.py: add function to convert OpenCV image to ImageTk.

--- artificialpicasso/imgutils.py
+++ artificialpicasso/imgutils.py
@@ -1,4 +1,6 @@
 import numpy as np
+import cv2
+from PIL import Image, ImageTk
 
 
 def filled_image(dimensions: tuple[int, int, int], color: int) -> np.ndarray:
@@ -7,3 +9,7 @@ def filled_image(dimensions: tuple[int, int, int], color: int) -> np.ndarray:
 
 def white_image(dimensions: tuple[int, int, int]) -> np.ndarray:
     return filled_image(dimensions, 255)
+
+
+def cv2_image_to_image_tk(img):
+    return ImageTk.PhotoImage(Image.fromarray(cv2.cvtColor(img, cv2.COLOR_BGR2RGBA)))
